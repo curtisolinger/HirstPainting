a4a9236551f2824159bfb4f1aa0a1f5381bc34e4
Curtis Olinger <curtisolinger@gmail.com>
2023-02-07 16:42:35 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+import hirst_colors
+import random
+
+sam = turtle.Turtle()
+sam.shape("turtle")
+turtle.colormode(255)
+
+sam.goto(-250, -250)
+
+
+screen = turtle.Screen()
+screen.exitonclick()
